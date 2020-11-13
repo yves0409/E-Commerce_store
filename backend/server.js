b73ff745a0f4750b3f691cd230036c6175fd7cab
Yves Loeys <yves.loeys@gmail.com>
2020-11-13 12:51:26 +0100

    Added server and seeder js files

--- backend/server.js
+++ backend/server.js
@@ -4,31 +4,47 @@
 //SO INSTEAD OF 'module.exports' WE CAN JUST USE EXPORT DEFAULT
 //NOTE THAT IF WE IMPORT A FILE WE NEED TO USE THE .js EXTENSION
 import express from "express";
-import products from "./data/products.js";
+import colors from "colors";
+import connectDB from "./config/db.js";
 import dotenv from "dotenv";
-
-const app = express();
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
+connectDB();
+const app = express();
+
+//middleware that allows us to accept json data in the body
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("Server is running");
+  res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+//routes we get from from the controllers
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+//ROUTE TO PAYPAL CLIENT_ID
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
 
+//error handling middleware imported for middleware
+app.use(notFound);
+app.use(errorHandler);
+
+//env variable for the PORT where the server is running
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .underline
   )
 );
